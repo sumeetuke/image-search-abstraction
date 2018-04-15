7b3a80c27202847eeb36a2287bc16d04294fad62
sumeet <sumeetuke@rediffmail.com>
2018-04-15 10:48:42 +0530

    new one

--- sarver.js
+++ sarver.js
@@ -9,6 +9,19 @@ const Bing = require('node-bing-api')({accKey:'db63791f74064c3ca3a65570902162f5'
 app.use(bodyParesr.json());
 app.use(cors());
 
+app.get('/api/imagesearch/:searchVal*',(req, res, next)=>{
+var {searchVal} = req.params;
+var {offset} = req.require;
+
+res.json({
+
+  searchVal,
+  offset
+
+});
+});
+
+
 
 app.listen(process.env.PORT || 3000,()=>{
 console.log('surver is running')
